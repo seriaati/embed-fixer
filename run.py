a1325aa3c0ece8c9bcb64767e89f2bed746926bf
seriaati <seria.ati@gmail.com>
2024-02-14 09:57:38 +0900

    feat: Implement aiohttp caching

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ import contextlib
 import logging
 import os
 
-import aiohttp
 import discord
+from aiohttp_client_cache.backends.sqlite import SQLiteBackend
+from aiohttp_client_cache.session import CachedSession
 from dotenv import load_dotenv
 from seria.logging import setup_logging
 
@@ -18,7 +19,9 @@ discord.VoiceClient.warn_nacl = False
 
 
 async def main() -> None:
-    async with aiohttp.ClientSession() as session, EmbedFixer(session=session, env=env) as bot:
+    async with CachedSession(cache=SQLiteBackend(expire_after=60 * 60)) as session, EmbedFixer(
+        session=session, env=env
+    ) as bot:
         with contextlib.suppress(KeyboardInterrupt, asyncio.CancelledError):
             await bot.start(os.environ["DISCORD_TOKEN"])
 
