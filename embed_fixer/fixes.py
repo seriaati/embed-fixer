c1462922a60d08a33a3cb256c9c1ffeed95f4cb8
seria <seria.ati@gmail.com>
2024-04-12 15:49:12 +0900

    fix: Fix tiktok regex pattern
    Fixes #11

--- embed_fixer/fixes.py
+++ embed_fixer/fixes.py
@@ -2,7 +2,7 @@ FIX_PATTERNS = (
     r"https://twitter.com/[a-zA-Z0-9_]+/status/\d+",
     r"https://x.com/[a-zA-Z0-9_]+/status/\d+",
     r"https://www.pixiv.net(/[a-zA-Z]+)?/artworks/\d+",
-    r"https://www.tiktok.com/@[\w]+/video/\d+",
+    r"https://www.tiktok.com/(t/\w+|@[\w.]+/video/\d+)",
     r"https://www.reddit.com/r/[\w]+/comments/[\w]+/[\w]+",
     r"https://www.instagram.com/(p|reels?)/[\w]+",
 )
