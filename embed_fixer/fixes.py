a430accc9fa05363d38efc895cc4f45e35e2c02b
seria <seria.ati@gmail.com>
2024-10-31 08:47:28 +0900

    refactor: Remove unnecessary escape char

--- embed_fixer/fixes.py
+++ embed_fixer/fixes.py
@@ -1,21 +1,21 @@
 from __future__ import annotations
 
 FIX_PATTERNS = (
-    r"https://(www\.)?twitter.com/[a-zA-Z0-9_]+/status/\d+(/photo|video/\d+)?",
-    r"https://(www\.)?x.com/[a-zA-Z0-9_]+/status/\d+(/photo|video/\d+)?",
-    r"https://(www\.)?pixiv.net(/[a-zA-Z]+)?/artworks/\d+",
-    r"https://(www\.)?tiktok.com/(t/\w+|@[\w.]+/video/\d+)",
+    r"https://(www.)?twitter.com/[a-zA-Z0-9_]+/status/\d+(/photo|video/\d+)?",
+    r"https://(www.)?x.com/[a-zA-Z0-9_]+/status/\d+(/photo|video/\d+)?",
+    r"https://(www.)?pixiv.net(/[a-zA-Z]+)?/artworks/\d+",
+    r"https://(www.)?tiktok.com/(t/\w+|@[\w.]+/video/\d+)",
     r"https://vm.tiktok.com/\w+",
     r"https://vt.tiktok.com/\w+",
-    r"https://(www\.)?reddit.com/r/[\w]+/comments/[\w]+/[\w]+",
-    r"https://(www\.)?reddit.com/r/[\w]+/s/[\w]+",
-    r"https://(www\.)?instagram.com/(p|reels?)/[\w]+",
-    r"https://(www\.)?furaffinity.net/view/\d+",
+    r"https://(www.)?reddit.com/r/[\w]+/comments/[\w]+/[\w]+",
+    r"https://(www.)?reddit.com/r/[\w]+/s/[\w]+",
+    r"https://(www.)?instagram.com/(p|reels?)/[\w]+",
+    r"https://(www.)?furaffinity.net/view/\d+",
     r"https://m.twitch.tv/clip/[\w]+",
     r"https://clips.twitch.tv/[\w]+",
-    r"https://(www\.)?iwara.tv/video/[\w]+/[\w]+",
-    r"https://(www\.)?twitch.tv/[\w]+/clip/[\w]+",
-    r"https://(www\.)?bsky.app/profile/[\w.]+/post/[\w]+",
+    r"https://(www.)?iwara.tv/video/[\w]+/[\w]+",
+    r"https://(www.)?twitch.tv/[\w]+/clip/[\w]+",
+    r"https://(www.)?bsky.app/profile/[\w.]+/post/[\w]+",
 )
 FIXES = {
     "twitter.com": "fxtwitter.com",
