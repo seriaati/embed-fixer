fc241cdbcf93cc7ece215d2f63fdc2830a2457c3
seria <seria.ati@gmail.com>
2025-04-23 11:26:02 +0900

    fix(db): Add default value for migration

--- migrations/embed_fixer/6_20250423112143_update.py
+++ migrations/embed_fixer/6_20250423112143_update.py
@@ -3,7 +3,7 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "guild_settings" ADD "enable_fix_channels" JSONB NOT NULL;"""
+        ALTER TABLE "guild_settings" ADD "enable_fix_channels" JSONB NOT NULL DEFAULT '[]';"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
