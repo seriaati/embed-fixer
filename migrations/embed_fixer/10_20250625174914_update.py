8aab6df5dd1753abcabb581b2b2bc57ee1c8646f
seria <seria.ati@gmail.com>
2025-06-25 17:58:38 +0900

    Fix missing default value for column

--- migrations/embed_fixer/10_20250625174914_update.py
+++ migrations/embed_fixer/10_20250625174914_update.py
@@ -3,7 +3,7 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "guild_settings" ADD "whitelist_role_ids" JSONB NOT NULL;"""
+        ALTER TABLE "guild_settings" ADD "whitelist_role_ids" JSONB DEFAULT '[]' NOT NULL;"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
