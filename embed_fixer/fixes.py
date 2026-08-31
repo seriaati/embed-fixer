e73959772c490b1bda5ba01f981f551cb6240bb1
seria <seria.ati@gmail.com>
2026-08-31 23:37:53 +0800

    fix(facebook): match web.facebook.com URLs
    web.facebook.com is Facebook's zero-rating alias serving the same
    paths as www; URL patterns only matched (www.)?facebook.com so these
    links were ignored.
    
    Closes #321

--- embed_fixer/fixes.py
+++ embed_fixer/fixes.py
@@ -338,10 +338,10 @@ DOMAINS: Final[list[Domain]] = [
         id=DomainId.FACEBOOK,
         name="Facebook",
         websites=[
-            Website(r"https://(www.)?facebook.com/share/r/[\w]+/?"),
-            Website(r"https://(www.)?facebook.com/reel/\d+/?"),
-            Website(r"https://(www.)?facebook.com/share/v/[\w]+/?"),
-            Website(r"https://(www.)?facebook.com/(.*)", skip_method_ids=[15]),
+            Website(r"https://(www.|web.)?facebook.com/share/r/[\w]+/?"),
+            Website(r"https://(www.|web.)?facebook.com/reel/\d+/?"),
+            Website(r"https://(www.|web.)?facebook.com/share/v/[\w]+/?"),
+            Website(r"https://(www.|web.)?facebook.com/(.*)", skip_method_ids=[15]),
         ],
         fix_methods=[
             FixMethod(
